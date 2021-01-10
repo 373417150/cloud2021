d678152e070ca393b557bfc49b8c6c5082c7ad9d
dzf <373417150@qq.com>
2021-01-10 19:39:19 +0800

    3366config配置的客服端

---
+++ cloud-config-client-3366/src/main/java/com/configClient/springcloud/Main3366.java
@@ -0,0 +1,14 @@
+package com.configClient.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Main3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main3366.class, args);
+    }
+}
