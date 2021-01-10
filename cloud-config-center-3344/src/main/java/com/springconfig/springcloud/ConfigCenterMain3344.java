ea723016dc1c30722b4b2524a1b5ba628ef3e3cd
dzf <373417150@qq.com>
2021-01-10 19:27:07 +0800

    二次提交

--- cloud-config-center-3344/src/main/java/com/springconfig/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/springconfig/springcloud/ConfigCenterMain3344.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigCenterMain3344
 {
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigCenterMain3344.class, args);
     }
 }
